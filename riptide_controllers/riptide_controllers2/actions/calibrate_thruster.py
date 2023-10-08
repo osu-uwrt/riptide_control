de1085203267868d92ac419b10d0e1b9885350d3
Nathan Becker <beckernm@gmail.com>
2023-10-07 20:27:26 -0400

    Made data collection work with rpm and force subs

--- riptide_controllers/riptide_controllers2/actions/calibrate_thruster.py
+++ riptide_controllers/riptide_controllers2/actions/calibrate_thruster.py
@@ -1,13 +1,24 @@
 #! /usr/bin/env python3
 
+"""
+NOTES FROM NATHAN:
+- Need to change RPM subscriber name and type on line 59; I don't know what it's supposed to be
+- I assumed the RPM message was stored as a list, so I accessed the correct thruster in callback with index, change if assumption was garbo
+- Change constants as desired
+- I have a MATLAB script to curve fit the data to any function imaginable and to do other data processing from csv file. Please send me data once collected
+- Pizza is delecious
+"""
+
 import time
 from datetime import datetime
+from statistics import mean
 import csv
 import yaml
 import os
 
 import rclpy
 from rclpy.action import ActionServer, GoalResponse, CancelResponse
+from std_msgs.msg import Empty, Int32
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
@@ -16,7 +27,11 @@ from rclpy.qos import qos_profile_system_default, qos_profile_sensor_data
 from riptide_msgs2.action import CalibrateThruster
 from riptide_msgs2.msg import DshotCommand
 
-NUETRAL_DSHOT = 0
+NUETRAL_DSHOT = 0           # Dshot value which is "off" for the thrusters
+DSHOT_PUB_PERIOD = 0.075    # Time in seconds between dshot timer publishes
+DELAY_TIME = 0.5            # Delay in seconds between sending command and collecting data
+N_SAMPLES = 15              # Number of samples to collect at each dshot value
+COLLECTION_TIMEOUT = 15     # Allowed data collection time in seconds before timeout
 
 
 class CalibrateThrusterAction(Node):
@@ -25,47 +40,65 @@ class CalibrateThrusterAction(Node):
         # Create Node
         super().__init__('calibrate_thruster')
 
+        # Variables
         self.running = False
-        self.dshot = 0
+        self.collecting_data = False
+        self.triggered = False
+        self.dshot = NUETRAL_DSHOT
+        self.thruster_length = self.get_thruster_length()
         self.thruster_num = 0
+        self.rpm = []
+        self.force = []
 
+        # Create publishers and subscribers
         self.dshot_pub = self.create_publisher(
             DshotCommand, "command/dshot", qos_profile_sensor_data)
+        self.trigger_sub = self.create_subscription(
+            Empty, "command/trigger", self.trigger_callback, qos_profile_system_default)
+        self.thruster_rpm_sub = self.create_subscription(
+            Empty, "ENTER_RPM_TOPIC_NAME_HERE", self.rpm_callback, qos_profile_sensor_data)
+        self.thruster_force_sub = self.create_subscription(
+            Int32, "force_gauge/force", self.force_callback, qos_profile_sensor_data)
 
+        # Create action server
         self._action_server = ActionServer(
             self,
             CalibrateThruster,
             'calibrate_thruster',
-            self.collect_data,
+            self.collect_data_callback,
             goal_callback=self.goal_callback,
             cancel_callback=self.cancel_callback,
             callback_group=ReentrantCallbackGroup())
 
         # Create timer to keep publishing the dshot command to prevent firmware from timing out
-        self.timer = self.create_timer(0.075, self.publish_dshot_command)
+        self.timer = self.create_timer(
+            DSHOT_PUB_PERIOD, self.publish_dshot_command)
 
-    def collect_data(self, goal_handle):
+    def collect_data_callback(self, goal_handle):
+        # Method walks data through collection process and saves the result to csv file
+
         self.thruster_num = goal_handle.request.thruster_num
         feedback_msg = CalibrateThruster.Feedback()
 
-        self.get_logger().info('Press any key to tare load cell:')
-        wait_for_input = input()
-        ##############################
-        ####  ethans_code.tare()  ####
-        ##############################
+        # Wait for user to tare scale by reseting the arduino
+        self.get_logger().info(
+            'Please reset arduino to tare the scale, publish command/trigger once complete')
+        self.wait_for_input()
 
         # Start calibration, looping through dshot values and saving data
         self.get_logger().info('Thruster calibration starting...')
         flag = True
         rows_data = []
+        flip_force = -1
         for dshot_value in range(DshotCommand.DSHOT_MIN, DshotCommand.DSHOT_MAX, goal_handle.request.step_size):
-            # Once egative dshot commands have finished, operator needs to flip thruster before continueing
+            # Once negative dshot commands have finished, operator needs to flip thruster before continueing
             if dshot_value > NUETRAL_DSHOT and flag:
                 flag = False
-                self.get_logger().info('Please flip thruster, press any key once complete:')
-                wait_for_input = input()
+                flip_force = 1
+                self.get_logger().info('Please flip thruster, publish command/trigger once complete')
+                self.wait_for_input()
 
-            # Send dshot command to thruster ### DO I NEED A -1??????
+            # Send dshot command to thruster
             self.dshot = dshot_value
             self.publish_dshot_command()
 
@@ -75,15 +108,12 @@ class CalibrateThrusterAction(Node):
             self.get_logger().info(
                 f"Current dshot value: {dshot_value}")
 
-            # Delay for transients to setting and for thruster to respond
-            time.sleep(0.5)
+            # Delay for transients to settle and for thruster to respond
+            time.sleep(DELAY_TIME)
 
             # Collect data and store it
-            ##################################
-            ####  ethans_code.get_data()  ####
-            ##################################
-            (avg_force, avg_rpm) = (0, 0)
-            rows_data.append([dshot_value, avg_rpm, avg_force])
+            (avg_rpm, avg_force) = self.get_data(N_SAMPLES)
+            rows_data.append([dshot_value, avg_rpm, flip_force*avg_force])
 
         # Data collection finished, send command to turn off thruster
         self.dshot = NUETRAL_DSHOT
@@ -93,36 +123,35 @@ class CalibrateThrusterAction(Node):
         result = CalibrateThruster.Result()
         result.results_file_name = self.save_data(rows_data)
         if result.results_file_name:
-            goal_handle.succeed(result)
+            goal_handle.succeed()
         else:
-            goal_handle.abort(result)
+            goal_handle.abort()
         self.running = False
+        self.get_logger().info(result.results_file_name)
+        return result
 
-    def goal_callback(self, goal_request):
-        # Accept or reject a client request to begin an action
-        if self.running:
-            return GoalResponse.REJECT
-        else:
-            self.running = True
-            return GoalResponse.ACCEPT
+    def get_data(self, N_samples):
+        # Method starts data collect, and returns average results after N_samples have been collected
+        # Once collecting_data is True, the rpm and force lists will get appended by subscriber callbacks
 
-    def cancel_callback(self, goal):
-        return CancelResponse.ACCEPT
+        # Clear previous data and start collecting
+        self.rpm = []
+        self.force = []
+        self.collecting_data = True
 
-    def destroy(self):
-        self._action_server.destroy()
-        super().destroy_node()
+        # Wait for samples to collect, report error if the collection is taking too long
+        start_time = time.time()
+        while rclpy.ok() and min(len(self.rpm), len(self.force)) < N_samples:
+            if time.time()-start_time > COLLECTION_TIMEOUT:
+                self.get_logger().error(
+                    f"Data collection timed out at dshot value {self.dshot}")
+                # Data failed to collect, return imaginary numbers to indicate failure
+                return (1j, 1j)
+        # Stop data collection and return averages
+        self.collecting_data = False
+        return (mean(self.rpm), mean(self.force))
 
-    def publish_dshot_command(self):
-        # Publishes dshot value to specified thruster and 0 to all other thrusters
-        # Function is called on timer to prevent firmware from timing out
-        dshot_msg = DshotCommand()
-        dshot_values = self.zeros_thruster_len()
-        dshot_values[self.thruster_num] = self.dshot
-        dshot_msg.values = dshot_values
-        self.dshot_pub.publish(self.dshot_msg)
-
-    def save_data(self, data):
+    def save_data(self, data) -> str:
         # Saves each row of data into a csv file and returns the file name
         try:
             # Create unique file name to store data to
@@ -146,7 +175,7 @@ class CalibrateThrusterAction(Node):
         # Creates unique filename from current date and time
         base_filename = f"thruster{self.thruster_num}_cal_data"
         current_datetime = datetime.now()
-        date_string = current_datetime.strftime("%Y-%m-%d-%H-%M")
+        date_string = current_datetime.strftime("%Y-%m-%d-%H-%M-%S")
 
         # Get the user's home directory
         home_dir = os.path.expanduser("~")
@@ -158,18 +187,72 @@ class CalibrateThrusterAction(Node):
         # Return joined file name in path
         return os.path.join(folder_path, f"{base_filename}_{date_string}.csv")
 
-    def zeros_thruster_len(self):
+    def get_thruster_length(self) -> int:
+        # Returns amount of thrusters on vehicle
         # Load thruster info
+        """
         self.declare_parameter("vehicle_config", "")
         config_path = self.get_parameter("vehicle_config").value
         if (config_path == ''):
-            self.get_logger().fatal("vehicle config file param not set or empty, exiting")
+            self.get_logger().fatal(
+                "vehicle config file param not set or empty, using assumed thruster count of 8")
+            return 8
         with open(config_path, 'r') as stream:
             config_file = yaml.safe_load(stream)
-        thruster_info = config_file['thrusters']
+        thruster_info = config_file['thrusters']"""
+        return 8  # len(thruster_info)
 
-        # Return zero list that is the number of thrusters long
-        return [0]*len(thruster_info)
+    def wait_for_input(self):
+        # Goes into loop until Node has recieved a trigger command
+        self.triggered = False
+        while rclpy.ok() and not self.triggered:
+            pass
+
+    #######################################
+    #    Callback and timer functions     #
+    #######################################
+
+    def publish_dshot_command(self):
+        # Publishes dshot value to specified thruster and NUETRAL_DSHOT to all other thrusters
+        # Function is called on timer to prevent firmware from timing out
+        dshot_msg = DshotCommand()
+        dshot_values = [NUETRAL_DSHOT]*self.thruster_length
+        dshot_values[self.thruster_num] = self.dshot
+        dshot_msg.values = dshot_values
+        self.dshot_pub.publish(dshot_msg)
+
+    def trigger_callback(self, msg: Empty):
+        # Indicated user has triggered to move on in code
+        self.triggered = True
+
+    def rpm_callback(self, msg):
+        # Adds rpm data to list if data is collecting
+        if self.collecting_data:
+            self.rpm.append(msg[self.thruster_num])
+
+    def force_callback(self, msg: Int32):
+        # Adds force data to list if data is collecting
+        if self.collecting_data:
+            self.force.append(msg)
+
+    ##########################################
+    #     Boiler plate action functions      #
+    ##########################################
+
+    def goal_callback(self, goal_request):
+        # Accept or reject a client request to begin an action
+        if self.running:
+            return GoalResponse.REJECT
+        else:
+            self.running = True
+            return GoalResponse.ACCEPT
+
+    def cancel_callback(self, goal):
+        return CancelResponse.ACCEPT
+
+    def destroy(self):
+        self._action_server.destroy()
+        super().destroy_node()
 
 
 def main(args=None):
---
+++ riptide_controllers/riptide_controllers2/actions/load_cell_reader.py
@@ -0,0 +1,93 @@
+import sys
+import rclpy
+import serial
+
+from std_msgs.msg import Int32
+from rclpy.node import Node
+from rclpy.qos import qos_profile_system_default
+
+
+class SerialDecoder:
+    def __init__(self):
+        self.reset_state()
+
+    def reset_state(self):
+        self._state = 0
+        self._idx = 0
+        self._valid = False
+
+    def process_byte(self, val: bytes):
+        assert len(val) == 1, "Byte must be single length"
+        val = val[0]
+        # Handle start char
+        if val & 0x80:
+            self.reset_state()
+            val &= 0x7F
+        elif self._idx == 0:
+            # Don't process packets if we need a start character
+            print("Waiting for sync...")
+            return
+
+        # Don't process any more bytes after 4
+        if self._idx > 3:
+            print("Garbage data received after packet, waiting for sync")
+            return
+        elif self._idx == 3:
+            # If third byte, handle checksum special
+            checksum = val >> 3
+            val &= 0x7
+            self._state |= val << (8 * self._idx)
+            self._idx += 1
+
+            computed_checksum = 0b1011
+            tmp_state = self._state
+            for _ in range(24//4):
+                computed_checksum ^= tmp_state & 0xF
+                tmp_state >>= 4
+
+            self._valid = (computed_checksum == checksum)
+            if not self._valid:
+                print("Failed to decode, invalid packet ({} vs {})".format(
+                    hex(computed_checksum), hex(checksum)))
+                self.reset_state()
+        else:
+            self._state |= val << (8 * self._idx)
+            self._idx += 1
+
+    def available(self) -> bool:
+        return self._valid
+
+    def get_value(self) -> bool:
+        if not self._valid:
+            raise RuntimeError("No valid reading available")
+
+        if self._state < 0x80_0000:
+            return self._state
+        else:
+            # Perform twos-complement on result
+            return -1 * (0x100_0000 - self._state)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Node('thruster_force_gauge_publisher')
+    node.declare_parameter("port", "/dev/ttyACM0")
+    pub = node.create_publisher(
+        Int32, "force_gauge/force", qos_profile_system_default)
+
+    ser = serial.Serial(node.get_parameter("port").value, 115200)
+    decoder = SerialDecoder()
+
+    while True:
+        while ser.in_waiting:
+            decoder.process_byte(ser.read(1))
+            if decoder.available():
+                pub.publish(Int32(data=decoder.get_value()))
+        # If you change the publish rates on the arduino, adjust this
+        rclpy.spin_once(node, timeout_sec=0.005)
+
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
