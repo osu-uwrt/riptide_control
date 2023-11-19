8fd6a38d60ed96397b6e78a9fec2562880dd1aa7
Brach Knutson <brach2003@gmail.com>
2023-11-19 15:58:02 -0500

    have ceval calling the library properly

--- riptide_controllers/models/active_control/generate_motion_profile.m
+++ riptide_controllers/models/active_control/generate_motion_profile.m
@@ -3,12 +3,15 @@
 % state. curr_state and desired_state are both input as vectors in the
 % order: [x, y, z, r, p, w, vx, vy, vz, vr, vp, vw]
 %
-function profiles = generate_motion_profile(curr_state, desired_state)
+function profiles = generate_motion_profile( ...
+    curr_state, desired_state, vmax, amax, jmax, time_step)
     if coder.target("MATLAB")
-        profiles = generate_with_mex(curr_state, desired_state);
+        profiles = generate_with_mex( ...
+            curr_state, desired_state, vmax, amax, jmax, time_step);
     else
-        profiles = generate_with_ceval(curr_state, desired_state);
-    end
+        profiles = generate_with_ceval( ...
+            curr_state, desired_state, vmax, amax, jmax, time_step);
+    end 
 end
 
 
@@ -17,8 +20,10 @@ end
 % profile. Because MEX cannot be compiled to C++, this function would only
 % be used when debugging IN LOOP
 % 
-function profiles = generate_with_mex(curr_state, desired_state)
-
+function profiles = generate_with_mex( ...
+    curr_state, desired_state, vmax, amax, jmax, time_step)
+    profiles = zeros(4, 4);
+    fprintf("mex not supported yet\n");
 end
 
 
@@ -27,6 +32,22 @@ end
 % Because ceval cannot be used to debug in loop, this function will only be
 % used when running generated code.
 %
-function profiles = generate_with_ceval(curr_state, desired_state)
+function profiles = generate_with_ceval( ...
+    curr_state, desired_state, vmax, amax, jmax, time_step)
+    
+    DEGREES_OF_FREEDOM = 6;
+    profile_length = 0;
 
+    coder.updateBuildInfo("addSourceFiles", "profiler_matlab_interface.cpp")
+    
+    profile_length = coder.ceval("profiler_invoke", ...
+        curr_state, desired_state, vmax, amax, jmax, time_step)
+
+    profiles = zeros(DEGREES_OF_FREEDOM, profile_length);
+
+    for i = 1 : DEGREES_OF_FREEDOM
+        coder.ceval("profiler_get", i, coder.wref(profiles(i)));
+    end
+
+    profiles
 end
--- riptide_controllers/src/riptide_controllers/main.cpp
+++ riptide_controllers/src/riptide_controllers/main.cpp
@@ -7,7 +7,14 @@
 #define DEGREES_OF_FREEDOM 6
 #define STATE_ARRAY_SIZE (DEGREES_OF_FREEDOM * 2)
 
-double profiles[DEGREES_OF_FREEDOM][10];
+static double profiles[DEGREES_OF_FREEDOM][10];
+static double profile[10] = {0};
+static bool initialized = false;
+static double
+    stored_vmax[DEGREES_OF_FREEDOM],
+    stored_amax[DEGREES_OF_FREEDOM],
+    stored_jmax[DEGREES_OF_FREEDOM],
+    stored_time_step;
 
 
 //this is a good test to see if linking working
@@ -31,7 +38,13 @@ extern "C" void profiler_init(
     double jmax[DEGREES_OF_FREEDOM], 
     double timeStep)
 {
+    initialized = true;
+}
 
+
+extern "C" bool profiler_initialized()
+{
+    return initialized;
 }
 
 
@@ -41,26 +54,44 @@ extern "C" void profiler_set_constraints(
     double jmax[DEGREES_OF_FREEDOM],
     double timeStep)
 {
-
+    memcpy(stored_vmax, vmax, DEGREES_OF_FREEDOM);
+    memcpy(stored_amax, amax, DEGREES_OF_FREEDOM);
+    memcpy(stored_jmax, jmax, DEGREES_OF_FREEDOM);
+    stored_time_step = timeStep;
 }
 
 
-extern "C" void profiler_calculate_new_profile(
+extern "C" void profiler_get_constraints(
+    double vmax[DEGREES_OF_FREEDOM],
+    double amax[DEGREES_OF_FREEDOM],
+    double jmax[DEGREES_OF_FREEDOM],
+    double *time_step)
+{
+    memcpy(vmax, stored_vmax, DEGREES_OF_FREEDOM);
+    memcpy(amax, stored_amax, DEGREES_OF_FREEDOM);
+    memcpy(jmax, stored_jmax, DEGREES_OF_FREEDOM);
+    *time_step = stored_time_step;
+}
+
+
+extern "C" int profiler_calculate_new_profile(
     double current_state[STATE_ARRAY_SIZE], 
     double target_state[STATE_ARRAY_SIZE])
 {
-    for(int i = 0; i < DEGREES_OF_FREEDOM; i++)
-    {
-        for(int j = 0; j < 10; j++)
-        {
-            profiles[i][j] = rand();
-        }
-    }
+    // for(int i = 0; i < DEGREES_OF_FREEDOM; i++)
+    // {
+    //     for(int j = 0; j < 10; j++)
+    //     {
+    //         profiles[i][j] = rand();
+    //     }
+    // }
+
+    return 10;
 }
 
 
-extern "C" void profiler_get_profile(int index, double *out, int *length)
+extern "C" void profiler_get_profile(int index, double *out)
 {
-    memcpy(out, profiles[index], 10);
-    *length = 10;
+    // memcpy(out, profiles[index], 10);
+    memcpy(out, profile, 10);
 }
