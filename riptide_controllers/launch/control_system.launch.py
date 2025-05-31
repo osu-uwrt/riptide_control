dca67a6ec74f204a8894605ceab6d6a332ba747d
osu_uwrt_bot <osu.uwrt@gmail.com>
2025-05-31 15:44:44 -0400

    liltank controller launch

--- riptide_controllers/launch/control_system.launch.py
+++ riptide_controllers/launch/control_system.launch.py
@@ -15,9 +15,17 @@ def get_complete_controller_launch(launch_prefix):
     )
     
 def get_liltank_controller_launch(launch_prefix):
-    return None
+    return Node(
+        prefix=[launch_prefix],
+        package="liltank_controller",
+        executable="liltank_controller",
+        name="liltank_controller",
+        output="screen"
+    )
 
-def get_launch_prefix():
+def get_launch_prefix(context):
+    if LaunchConfiguration("core_reservation_enabled").perform(context) != "True":
+        return ""
     
     #detect if we are running on the orin
     launch_prefix = ""
@@ -30,16 +38,31 @@ def get_launch_prefix():
     return launch_prefix
     
 
-def launch_active_control(context, *args, **kwargs):
+def launch_control_system(context, *args, **kwargs):
+    launch_prefix = get_launch_prefix(context)
+    
+    Node(
+        prefix=[launch_prefix],
+        package="riptide_controllers2",
+        executable="controller_overseer.py",
+        name="controller_overseer",
+        parameters = [
+            {
+                "vehicle_config": "",  # Leave empty to let the node discover it
+                "robot": LaunchConfiguration("robot"),
+            }
+        ],
+        output="screen"
+    ),
+    
     active_control_enabled = LaunchConfiguration("active_control_enabled").perform(context)   
 
     if active_control_enabled == "True":
         robot = LaunchConfiguration("robot").perform(context)
         if robot == "talos":
-            return [get_complete_controller_launch(get_launch_prefix())]
+            return [get_complete_controller_launch(get_launch_prefix(context))]
         elif robot == "liltank":
-            # return [get_liltank_controller_launch(get_launch_prefix())]
-            return [] #TODO: replace with above line when controller is implemented
+            return [get_liltank_controller_launch(get_launch_prefix(context))]
     
     print("-----------------------------------------------------------------")
     print("Active control model either unknown or disabled, or robot name is unknown. Not launching.")
@@ -47,8 +70,6 @@ def launch_active_control(context, *args, **kwargs):
     return []
 
 def generate_launch_description():
-    launch_prefix = get_launch_prefix()
-    
     return LaunchDescription([
         DeclareLaunchArgument(name="robot", default_value="tempest",
                               description="name of the robot to run"),
@@ -58,24 +79,15 @@ def generate_launch_description():
         
         DeclareLaunchArgument(name="active_control_enabled", default_value="True",
                               description="Whether or not the active control model should be launched"),
+        
+        DeclareLaunchArgument(name="core_reservation_enabled", default_value="True",
+                              description="Whether or not to reserve a core on Jetson computers"),
 
         GroupAction([
             PushRosNamespace(LaunchConfiguration("robot")),
             
-            Node(
-                prefix=[launch_prefix],
-                package="riptide_controllers2",
-                executable="controller_overseer.py",
-                name="controller_overseer",
-                parameters = [
-                    {
-                        "vehicle_config": "",  # Leave empty to let the node discover it
-                        "robot": LaunchConfiguration("robot"),
-                    }
-                ],
-                output="screen"
-            ),
+            
 
-            OpaqueFunction(function=launch_active_control)
+            OpaqueFunction(function=launch_control_system)
         ], scoped=True)
     ])
