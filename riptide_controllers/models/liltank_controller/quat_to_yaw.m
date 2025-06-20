f2560f00c87bcb378642ac3d52c8a4922e45cb62
Brach Knutson <brach2003@gmail.com>
2025-06-20 00:51:37 -0400

    good enough liltank controller for autonomy

--- riptide_controllers/models/liltank_controller/quat_to_yaw.m
+++ riptide_controllers/models/liltank_controller/quat_to_yaw.m
@@ -1,5 +1,4 @@
-function yaw = quat_to_yaw(quat_bus)
-    quat = [quat_bus.w, quat_bus.x, quat_bus.y, quat_bus.z];
+function yaw = quat_to_yaw(quat)
     rotm = quat2rotm(quat);
     
     unit = [1, 0, 0];
